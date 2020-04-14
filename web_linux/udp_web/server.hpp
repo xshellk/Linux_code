5e44102ca621baf8419e229062b56e71dc80aa3d
xshellk <1120030398@example.com>
2020-04-14 23:27:49 +0800

    将server的头文件构造函数写完

--- web_linux/udp_web/server.hpp
+++ web_linux/udp_web/server.hpp
@@ -3,8 +3,50 @@
 #include<iostream>
 #include<sys/types.h>
 #include<sys/socket.h>
+#include<arpa/inet.h>
+#include<strings.h>
+
 #include<string>
 
+class server
+{
+  private:
+    short port;
+    std::string ip;
+  public:
+    server(short _port = 8080,std::string _ip = "127.0.0.1") : port(_port),ip(_ip)
+    {
 
+    }
+    void InitServer()
+    {
+      int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+      if(sockfd < 0)
+      {
+        std::cerr<< "socket error" << std::endl;
+        exit(2);
+      }
+      struct sockaddr_in local;
+      bzero(&local,sizeof(local));
+
+      local.sin_family = AF_INET;
+      local.sin_port = htons(port);
+      local.sin_addr.s_addr = inet_addr(ip.c_str());
+
+      if(bind(sockfd,(struct sockaddr*)&local,sizeof(local)) == 0)
+      {
+        std::cout << "run fun" << std::endl;
+      }
+      else{
+        std::cerr << "bind error" << std::endl;
+        exit(3);
+      }
+
+    }
+    ~server()
+    {
+
+    }
+};
 
 
