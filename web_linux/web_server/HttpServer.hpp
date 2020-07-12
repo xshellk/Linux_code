f44c270d4f899e9e6176cc13a45f6d0fecdc4328
xshellk <1120030398@example.com>
2020-07-12 21:56:24 +0800

    基本的从缓冲区读取request完成

--- web_linux/web_server/HttpServer.hpp
+++ web_linux/web_server/HttpServer.hpp
@@ -1,21 +1,49 @@
 #pragma once 
 
 #include"Sock.hpp"
+#include"Protocol.hpp"
 #include<iostream>
+#include<pthread.h>
+
+#define Default_port 8081
 
 using namespace std;
 
-class HttpServer
-{
+class HttpServer{
+  private:
+    short port;
+    int listen_sock;
+  
   public:
-    void Start(short port)
+    HttpServer(int _port = Default_port):port(_port),listen_sock(-1)
+    {}
+    ~HttpServer()
+    {}
+  
+  public:
+    void InitServer()
     {
-      int sockfd = Sock::CreatSock();
-      Sock::Bind(sockfd,port);
-      Sock::Listen(sockfd);
+      listen_sock = Sock::CreatSock();
+      Sock::Bind(listen_sock,port);
+      Sock::Listen(listen_sock);
+    }
+    void Start()
+    {
       for(;;)
       {
-        int sock = Sock::Accept(sockfd);
+        int sock = Sock::Accept(listen_sock);
+        if(sock > 0)
+        {
+          LOG(Normal,"Get a new link");
+          pthread_t tid;
+          pthread_create(&tid,nullptr,Entry::HanderRequest,(void *)sock);
+          pthread_detach(tid);
+
+        }
+        else 
+        {
+
+        }
       }
     }
 };
